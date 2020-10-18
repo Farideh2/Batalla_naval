de0d98fcb13e3adbe39a73d06c980fcf655e7dbe
aranis23 <rigeldjg@gmail.com>
2020-10-17 22:49:14 -0500

    Done

--- batalla_naval.py
+++ batalla_naval.py
@@ -10,13 +10,17 @@ coordenadas,ships = [],[]
 jugador1, jugador2 = [],[]
 player = 0
 visible1, visible2 = [],[]
+carga = 0
 
 #se crean los objetos para los efectos de sonidos
 explosion = "Explosion+9.wav"
-explosionObj = sa.WaveObject.from_wave_file(explosion)
+explosion_obj = sa.WaveObject.from_wave_file(explosion)
 
 chapoteo = "Cannonball-Splash-A2-www.fesliyanstudios.com (online-audio-converter.com).wav"
-chapoteoObj = sa.WaveObject.from_wave_file(chapoteo)
+chapoteo_obj = sa.WaveObject.from_wave_file(chapoteo)
+
+misil = "Missile+3.wav"
+misil_obj = sa.WaveObject.from_wave_file(misil) 
 
 #aqui se declaran unas variables para crear los tableros visibles a los jugadores con coordenadas
 visible1.append(["0","1","2","3","4","5","6","7","8","9","10"])
@@ -110,106 +114,111 @@ def cargar():
 
 #este metodo se corre cada vez que que los jugadrores quieran jugar y es donde se efectua el juego
 def jugador(koordinaten,jugador, barcos):
-    mensaje = "ingrese donde quiere atacar jugador #"+str(jugador)+": "
-    
+    mensaje = "ingrese donde quiere atacar jugador #" +str(jugador)+" en formato columna y renglon separado por comas (x,y): "
 
     global victory
     victory = 0
 
     #se usa el while para repitirt el input si es que la coordenada ya se añadio
     while True:
-        p = input(mensaje)
-        player = p.split(",")
 
-        x = int(player[0])
-        y = int(player[1])
-        
-        #checa si la coordenada ya se añadio
-        if jugador == 1  and (visible1[y][x] == "x" or visible1[y][x] == "0"):
-            print("Esa coordenada ya se añadio")
+        try:
+            p = input(mensaje)
+            player = p.split(",")
 
-        elif jugador == 2 and (visible2[y][x] == "x" or visible2[y][x] == "0"):
-            print("esta coordenada ya se añadio")
-
-        #aqui checa si impacto o no (es lo mismo en el resto de elif)
-        elif "1" in koordinaten[y-1][x-1] :
-            print("Averiado")
-            barcos[0] -= 1
-            if jugador == 1: 
-                visible1[y][x] = "x"
-                printear(visible1)
-            else: 
-                visible2[y][x] = "x"
-                printear(visible2)
+            x = int(player[0])
+            y = int(player[1])
             
-            if barcos[0]== 0:
-                print("Hundido")
-                #reporduce el sonido de un barco hundiendose
-                playExp = explosionObj.play()
-            break
+            #checa si la coordenada ya se añadio
+            if jugador == 1  and (visible1[y][x] == "x" or visible1[y][x] == "0"):
+                print("Esa coordenada ya se añadio")
+
+            elif jugador == 2 and (visible2[y][x] == "x" or visible2[y][x] == "0"):
+                print("esta coordenada ya se añadio")
+
+            #aqui checa si impacto o no (es lo mismo en el resto de elif)
+            elif "1" in koordinaten[y-1][x-1] :
+                print("Averiado")
+                barcos[0] -= 1
+                play_misil = misil_obj.play()
+                if jugador == 1: 
+                    visible1[y][x] = "x"
+                    printear(visible1)
+                else: 
+                    visible2[y][x] = "x"
+                    printear(visible2)
+                
+                if barcos[0]== 0:
+                    print("Hundido")
+                    #reporduce el sonido de un barco hundiendose
+                    playExp = explosion_obj.play()
+                break
 
 
-        elif "2" in koordinaten[y-1][x-1]:
-            print("acertaste")
-            barcos[1] -= 1
-            if jugador == 1: 
-                visible1[y][x] = "x"
-                printear(visible1)
-            else: 
-                visible2[y][x] = "x"
-                printear(visible2)
-            
-            if barcos[1]== 0:
-                print("hundiste el barco 2")
-                playExp = explosionObj.play()
-            break
+            elif "2" in koordinaten[y-1][x-1]:
+                print("acertaste")
+                barcos[1] -= 1
+                play_misil = misil_obj.play()
+                if jugador == 1: 
+                    visible1[y][x] = "x"
+                    printear(visible1)
+                else: 
+                    visible2[y][x] = "x"
+                    printear(visible2)
+                
+                if barcos[1]== 0:
+                    print("hundiste el barco 2")
+                    playExp = explosion_obj.play()
+                break
 
 
-        elif "3" in koordinaten[y-1][x-1]:
-            print("acertaste")
-            barcos[2] -= 1
-            if jugador == 1: 
-                visible1[y][x] = "x"
-                printear(visible1)
-            else: 
-                visible2[y][x] = "x"
-                printear(visible2)
+            elif "3" in koordinaten[y-1][x-1]:
+                print("acertaste")
+                barcos[2] -= 1
+                play_misil = misil_obj.play()
+                if jugador == 1: 
+                    visible1[y][x] = "x"
+                    printear(visible1)
+                else: 
+                    visible2[y][x] = "x"
+                    printear(visible2)
+                
+                if barcos[2]== 0:
+                    print("hundiste el barco 3")
+                    playExp = explosion_obj.play()
+                break
+                
+            elif "4" in koordinaten[y-1][x-1]:
+                print("acertaste")
+                barcos[3] -= 1
+                play_misil = misil_obj.play()
+                if jugador == 1: 
+                    visible1[y][x] = "x"
+                    printear(visible1)
+                else: 
+                    visible2[y][x] = "x"
+                    printear(visible2)
+                
+                if barcos[3]== 0:
+                    print("hundiste el barco 4")
+                    playExp = explosion_obj.play()
+                break
             
-            if barcos[2]== 0:
-                print("hundiste el barco 3")
-                playExp = explosionObj.play()
-            break
-            
-        elif "4" in koordinaten[y-1][x-1]:
-            print("acertaste")
-            barcos[3] -= 1
-            if jugador == 1: 
-                visible1[y][x] = "x"
-                printear(visible1)
-            else: 
-                visible2[y][x] = "x"
-                printear(visible2)
-            
-            if barcos[3]== 0:
-                print("hundiste el barco 4")
-                playExp = explosionObj.play()
-            break
-        
-        #indica si fallo
-        else:
-            print("fallaste")
-            if jugador == 1: 
-                visible1[y][x] = "0"
-                printear(visible1)
-                #reproduce el sonido de un fallo (chapoteo en el agua)
-                playExp = chapoteoObj.play()
-            else: 
-                visible2[y][x] = "0"
-                printear(visible2)
-                playExp = chapoteoObj.play()
-            break
-        
-    print(barcos)
+            #indica si fallo
+            else:
+                print("fallaste")
+                if jugador == 1: 
+                    visible1[y][x] = "0"
+                    printear(visible1)
+                    #reproduce el sonido de un fallo (chapoteo en el agua)
+                    playExp = chapoteo_obj.play()
+                else: 
+                    visible2[y][x] = "0"
+                    printear(visible2)
+                    playExp = chapoteo_obj.play()
+                break
+        except ValueError:
+            print("ingrese una coordenanada en fromato x,y separada por comas")
     return barcos
 
 #este metodo prepara todo para el metodo principal(jugador)  
@@ -226,14 +235,16 @@ def jugar(coordinates, botes):
     #unnjugador gana una vez que se hundan todos los barcos, para eso se resta numeros de la matriz de barcos
     #cada vez que se averia uno hasta que llegue a cero
     while True:
-        victoria1 = jugador(jugador2,1, barcos1)
- 
-        if sum(victoria1) == 0:
+        barcos1 = jugador(jugador2,1,barcos1)
+        print(barcos2)
+
+        if sum(barcos1) == 0:
             print("gano el jugador 1")
             break
 
-        victoria2 = jugador(jugador1,2, barcos2)
-        if sum(victoria2) == 0:
+        barcos2 = jugador(jugador1,2, barcos2)
+        print(barcos1)
+        if sum(barcos2) == 0:
             print("gano el jugador 1")
             break
 
@@ -245,14 +256,13 @@ while True:
     print("b)jugar")
     print("c)terminar")
 
-    #se usa esta variable para checar si ya se subio el archivo o no
-    carga = 0
-    
     opcion = input()
 
     if opcion == "a":
         coordenadas, ships = cargar()
-        cargar = 1
+        #se usa esta variable para checar si ya se subio el archivo o no  
+        carga = 1
+
     elif opcion == "b":
         if carga == 1:
             jugar(coordenadas, ships)
